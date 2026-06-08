485375e1b308f26250a8c478811ef96ab341b68a
Mohammed Kaif <mohamkaif2003@gmail.com>
2026-06-08 10:18:12 +0530

    Day 1

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+const connectDB = require("./config/db");
 require("dotenv").config();
+
 
 const app = express();
 
@@ -16,3 +18,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+connectDB();
